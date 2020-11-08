a06fb9b50bb8fb4e4eeda1f258640496a9707537
cyphred@stinkpad <jeremyzantua@gmail.com>
2020-11-08 18:43:59 +0800

    Migrated basic scripts to new scripts directory

---
+++ .scripts/scrot-notifier.sh
@@ -0,0 +1,7 @@
+# !/bin/sh
+# Scrot with Notifier
+# Takes a screenshot and sends a notification confirming the action
+# Jeremy Andrews Zantua | jeremyzantua@gmail.com
+
+datefname=$(date "+%Y-%m-%d-%H-%M-%S-%N")
+scrot $1 "$datefname"_$wx$h-scrot.png -e 'mv $f ~/Screenshots/ ; notify-send -u low "Screenshot taken" "Saved as $f"'
