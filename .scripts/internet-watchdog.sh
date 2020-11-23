0e795a7894d7d2251ffe482b92aeeb2dd472eb73
cyphred@stinkpad <jeremyzantua@gmail.com>
2020-11-23 09:01:09 +0800

    Created internet watchdog script
    When your internet connection is down, use this to notify you when your
    connection has been restored.

---
+++ .scripts/internet-watchdog.sh
@@ -0,0 +1,19 @@
+# !/bin/sh
+# Run this when your internet connection is acting shitty
+# This will notify you when the internet comes back on
+
+# The interval for checking in seconds
+interval=30
+
+# The address that will be pinged to check for internet
+addr="8.8.8.8"
+
+notify-send "Internet Watchdog started" "You will be notified when your internet has been restored."
+
+while true; do
+	# Check if a single ping receives a response
+	ping -c 1 $addr && break
+	sleep $interval
+done
+
+notify-send -u critical "You are back online" "A response from $addr has been received."
