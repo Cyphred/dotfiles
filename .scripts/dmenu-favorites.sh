b65ba53ffb3c1ee0dc5b4337fa21247cbb22d612
cyphred@stinkpad <jeremyzantua@gmail.com>
2020-11-23 08:49:17 +0800

    Fixed broken links in favorites menu

--- .scripts/dmenu-favorites.sh
+++ .scripts/dmenu-favorites.sh
@@ -11,15 +11,15 @@ case $INPUT in
 		$TERMINAL -e ranger ~/Data/school
 		;;
 	"books")
-		$TERMINAL -e ranger ~/Data/Documents/Books
+		$TERMINAL -e ranger ~/Data/Books
 		;;
 	"anime")
-		$TERMINAL -e ranger ~/Data/Downloads/Anime
+		$TERMINAL -e ranger ~/Data/Anime
 		;;
 	"tv")
-		$TERMINAL -e ranger ~/Data/Downloads/TV
+		$TERMINAL -e ranger ~/Data/TV
 		;;
 	"movies")
-		$TERMINAL -e ranger ~/Data/Downloads/Movies
+		$TERMINAL -e ranger ~/Data/Movies
 		;;
 esac
