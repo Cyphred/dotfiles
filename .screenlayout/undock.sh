d4c30093c6635f3e873f0ca02cb344a8eea22a89
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2021-02-07 19:17:16 +0800

    Changed screenlayout notification urgency

--- .screenlayout/undock.sh
+++ .screenlayout/undock.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 xrandr --output eDP1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off
 feh --bg-fill ~/.wallpaper
-notify-send -u low 'Screen layout changed' 'Set screen layout to Internal Display mode'
+notify-send 'Screen layout changed' 'Set screen layout to Internal Display mode'
