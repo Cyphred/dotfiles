1ea2187ff9090ed83f947fab4404adc6ec6cc38f
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2021-03-01 16:47:23 +0800

    Added option to browse school folders in favorites menu

--- .scripts/dmenu-shared-drive.sh
+++ .scripts/dmenu-shared-drive.sh
@@ -2,8 +2,21 @@
 # A simple menu for opening my shared drive's folders
 
 folders=( books dev documents downloads notes picutres school torrents )
-data_dir=/run/media/cyphred/Shared
+data_dir="/run/media/cyphred/Shared"
+
 input=$(echo "${folders[@]}" | sed 's/\ /\n/g' | dmenu)
 
-[ -z "$input" ] && exit
-[ -d "$data_dir/$input" ] && $TERMINAL -e ranger $data_dir/$input || notify-send "Directory \"$input\" not found"
+if [ -z "$input" ]; then
+	exit 1
+elif [ "$input" == "school" ]; then
+	school_dirs=( $(ls -d $data_dir/school/*/) )
+	school_dirs=( "${school_dirs[@]#"$data_dir"/school/}" )
+	school_dirs=( "${school_dirs[@]%/}" )
+	option=$(printf '%s\n' "${school_dirs[@]}" | dmenu -l 20)
+	$TERMINAL -e ranger $data_dir/school/$option
+
+elif [ -d "$data_dir/$input" ]; then
+	$TERMINAL -e ranger $data_dir/$input
+else
+	notify-send "Directory \"$input\" not found"
+fi
