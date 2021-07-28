7b2cd5024a26d2921346c5a9fed5b0e52502f9d3
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2021-07-28 23:07:08 +0800

    Minor fixes to statusbar output.

--- .scripts/screenrecord.sh
+++ .scripts/screenrecord.sh
@@ -1,1 +1,9 @@
-ffmpeg -f x11grab -r 30 -s 1920x1080 -i :0.0 -f alsa -i hw:1 out.mkv
+_usage() {
+	echo "Usage: screenrecord.sh <resolution> <output file name>\nFormat is mkv."
+	exit 1
+}
+
+[ -z $1 ] && _usage
+[ -z $2 ] && _usage
+
+ffmpeg -f x11grab -r 30 -s $1 -i :0.0 -f alsa -i hw:0 $2
