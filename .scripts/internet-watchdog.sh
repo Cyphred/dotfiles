3ff0d269172b35b3a3b910836fc70c16d177fcad
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2021-07-28 23:05:41 +0800

    Cleaned up internet watchdog

--- .scripts/internet-watchdog.sh
+++ .scripts/internet-watchdog.sh
@@ -2,25 +2,18 @@
 # Run this when your internet connection is acting shitty
 # This will notify you when the internet comes back on
 
+[ -z "$1" ] && echo "Please specify an interface." && exit 1
+if ! ip a | grep $1; then
+	echo "Inerface $1 does not exist."
+	exit 1
+fi
+interface=$1
+
 # The interval for checking in seconds
 interval=30
 
 # The address that will be pinged to check for internet
-addr="8.8.8.8"
-
-# Interface names
-wireless_interface="wlp4s0"
-ethernet_interface="enp0s25"
-
-# Ask what interface to use
-printf "What interface do you want to ping?\n1) ethernet\t2)wi-fi\n(default=1) "
-read interface
-[ -z "$interface" ] && interface=1
-case "$interface" in
-	"1") interface=$ethernet_interface ;;
-	"2") interface=$wireless_interface ;;
-	*) exit 1 ;;
-esac
+addr="1.1.1.1"
 
 notify-send "Internet Watchdog started" "You will be notified when your internet has been restored."
 
@@ -31,5 +24,3 @@ while true; do
 done
 
 notify-send -u critical "You are back online" "A response from $addr has been received."
-# Play sounds to catch your attention
-[ ! -z "$1" ] && mpv $1
