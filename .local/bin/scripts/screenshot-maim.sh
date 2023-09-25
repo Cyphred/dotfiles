7c92134bf69b5381eeb91212947acdb7e593bfbf
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-09-26 07:32:19 +0800

    Fixes main screenshot script

--- .local/bin/scripts/screenshot-maim.sh
+++ .local/bin/scripts/screenshot-maim.sh
@@ -3,7 +3,11 @@
 [ -z "$1" ] && echo "Please specify screenshot type (full, select, window)." && exit 1
 
 case $1 in:
-	"full")
+	"select")
+		OPTS="--select"
+		;;
+	"full)
+		OPTS="
 		
 filename="$HOME/pic-full-$(date '+%y%m%d-%H%M-%S').png"
 maim $filename && notify-send "Screenshot taken" "Saved screenshot as $filename"
