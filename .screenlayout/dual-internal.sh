aa0b18a67502d6633336fd2b3841f883312cb3e7
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-09-26 19:29:22 +0800

    Updated dual internal screen layout

--- .screenlayout/dual-internal.sh
+++ .screenlayout/dual-internal.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output LVDS1 --mode 1366x768 --pos 1920x156 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI3 --off --output VGA1 --off --output VIRTUAL1 --off
+
+xrandr --output LVDS1 --mode 1366x768 --pos 0x312 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VGA1 --primary --mode 1920x1080 --pos 1366x0 --rotate normal --output VIRTUAL1 --off
